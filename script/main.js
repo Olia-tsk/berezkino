d091aa258bd002fd8052b233dffe355062c07ab6
Olia Kurbatova <kurbatova.olia@gmail.com>
2025-03-29 05:29:14 +0700

    connect slider

--- script/main.js
+++ script/main.js
@@ -2,10 +2,18 @@ document.addEventListener("DOMContentLoaded", function () {
   Splide.defaults = {
     type: "loop",
   };
+
   var reviewsSlider = new Splide("#reviewsSlider", {
     pagination: false,
     perPage: 3,
     gap: "24px",
   });
   reviewsSlider.mount();
+
+  var articlesSlider = new Splide("#articlesSlider", {
+    pagination: false,
+    perPage: 4,
+    gap: "16px",
+  });
+  articlesSlider.mount();
 });
