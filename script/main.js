63431d66aaee5f8bf26bdded24d345d06c895845
Olia Kurbatova <kurbatova.olia@gmail.com>
2025-03-30 02:45:41 +0700

    add condition for launching slider

--- script/main.js
+++ script/main.js
@@ -2,18 +2,21 @@ document.addEventListener("DOMContentLoaded", function () {
   Splide.defaults = {
     type: "loop",
   };
+  if (document.getElementById("#reviewsSlider")) {
+    var reviewsSlider = new Splide("#reviewsSlider", {
+      pagination: false,
+      perPage: 3,
+      gap: "24px",
+    });
+    reviewsSlider.mount();
+  }
 
-  var reviewsSlider = new Splide("#reviewsSlider", {
-    pagination: false,
-    perPage: 3,
-    gap: "24px",
-  });
-  reviewsSlider.mount();
-
-  var articlesSlider = new Splide("#articlesSlider", {
-    pagination: false,
-    perPage: 4,
-    gap: "16px",
-  });
-  articlesSlider.mount();
+  if (document.getElementById("#articlesSlider")) {
+    var articlesSlider = new Splide("#articlesSlider", {
+      pagination: false,
+      perPage: 4,
+      gap: "16px",
+    });
+    articlesSlider.mount();
+  }
 });
